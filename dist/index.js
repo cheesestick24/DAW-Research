02d0fbd21e873f00f0c3ed7991f41301ea1476a8
ht17a075@oecu.jp <ht17a075@oecu.jp>
2019-10-17 14:17:55 +0900

    ピアノ作成

--- dist/index.js
+++ dist/index.js
@@ -1,3 +1,5 @@
+
+
 console.log("aaa");
 
 $(function () {
@@ -7,42 +9,60 @@ $(function () {
     autostart: true
   }).appendTo(document.body);
 
-  var rect = two.makeRectangle(two.width / 2, two.height / 2, 250, 250);
-  rect.noStroke().fill = getRandomColor();
+  let rect = [];
+  const scale = [
+    'C4',
+    'D4',
+    'E4',
+    'F4',
+    'G4',
+    'A4',
+    'B4',
+    'C5',
+    'D5',
+    'E5',
+    'F5',
+    'G5',
+    'A5',
+    'B5',
+  ];
+  for (let i = 0; i < scale.length; i++) {
+    rect[i] = two.makeRectangle(two.width / 5, two.height / 2, 50, 250);
+    two.width = two.width + 300;
+    rect[i].noStroke().fill = getRandomColor();
+    // two.height = two.height + 10;
+  }
 
   // Update the renderer in order to generate corresponding DOM Elements.
   two.update();
 
-  $(rect._renderer.elem)
-    .css('cursor', 'pointer')
-    .click(function (e) {
-      rect.fill = getRandomColor();
-      playTone();
-    });
+  for (let i = 0; i < scale.length; i++) {
+    $(rect[i]._renderer.elem)
+      .css('cursor', 'pointer')
+      .click(function (e) {
+        //rect.fill = getRandomColor();
+        playTone(i);
+      });
+  }
 
-  two.bind('update', function (frameCount, timeDelta) {
-    rect.rotation = frameCount / 60;
-  });
+  // two.bind('update', function (frameCount, timeDelta) {
+  //   rect.rotation = frameCount / 60;
+  // });
 
   function getRandomColor() {
-    console.log("color");
     return 'rgb('
       + Math.floor(Math.random() * 255) + ','
       + Math.floor(Math.random() * 255) + ','
       + Math.floor(Math.random() * 255) + ')';
   }
 
-  function playTone() {
-    console.log("in");
-
+  function playTone(index) {
     //鍵盤
     const synth = new Tone.Synth().toMaster();
-    console.log("make");
+
     //ピアノ音階作成
 
-    synth.triggerAttackRelease('C4', '8n');
-    console.log("play");
-
+    synth.triggerAttackRelease(scale[index], '8n');
   }
-  
+
 });
