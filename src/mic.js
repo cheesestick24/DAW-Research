b42a6e83237d3e0e5bff5b528e0266fe1a81086e
cheesestick24 <ht17a075@oecu.jp>
2019-10-19 00:11:50 +0900

    スライダー表示だけできた

--- src/mic.js
+++ src/mic.js
@@ -1,65 +1,65 @@
-// import Tone from 'tone'
-import * as recorder from './recorder.js'
+import Tone from 'tone'
+import * as recorder from "./recorder.js";
 
 const state = {
   recording: false,
   playing: false
-}
+};
 
 // import effect from './effect';
-$(function () {
-
+$(function() {
   var two = new Two({
     fullscreen: false,
     autostart: true
   }).appendTo(document.body);
 
-  let recbutton = two.makeRectangle(two.width / 2, two.height * 7 / 8, 100, 50);
-  recbutton.fill = '#FF8000';
+  let recbutton = two.makeRectangle(
+    two.width / 2,
+    (two.height * 7) / 8,
+    100,
+    50
+  );
+  recbutton.fill = "#FF8000";
 
-
-
-  let micstart = two.makeRectangle(two.width / 2, two.height * 6 / 8, 100, 50);
-  micstart.fill = '#ff0000';
-
-
+  let micstart = two.makeRectangle(
+    two.width / 2,
+    (two.height * 6) / 8,
+    100,
+    50
+  );
+  micstart.fill = "#ff0000";
 
   two.update();
 
   $(recbutton._renderer.elem)
-    .css('cursor', 'pointer')
-    .click(function (e) {
+    .css("cursor", "pointer")
+    .click(function(e) {
       //recbutton.fill = getRandomColor();
       if (state.recording === false) {
         console.log("start, recording: " + state.recording);
         recorder.recstart();
         state.recording = true;
-      }
-      else {
-        console.log("stop, recording: " + state.recording)
+      } else {
+        console.log("stop, recording: " + state.recording);
         recorder.recstop();
         state.recording = false;
       }
     });
 
-
-  const recordArray = recorder.recordArray
-
+  const recordArray = recorder.recordArray;
 
   const delay = new Tone.PingPongDelay({
-    'wet': 0
+    wet: 0
   }).toMaster();
 
   const reverb = new Tone.JCReverb({
-    'wet': 0
+    wet: 0
   }).toMaster();
-
-
 
   let playing = false;
   $(micstart._renderer.elem)
-    .css('cursor', 'pointer')
-    .click(function (e) {
+    .css("cursor", "pointer")
+    .click(function(e) {
       if (state.recording === false) {
         console.log("play + recording: " + state.recording);
         if (state.playing === false) {
@@ -76,39 +76,38 @@ $(function () {
       }
     });
 
-  let range = document.getElementById('range');
+    // // Initialize a new plugin instance for all
+    // // e.g. $('input[type="range"]') elements.
+    // $('input[type="range"]').rangeslider();
 
-  noUiSlider.create(range, {
-    start: [0],
-    step: [0.001],
-    connect: true,
-    orientation: 'horizontal',
-    behaviour: 'tap-drag',
-    range: {
-      'min': 0,
-      'max': 1,
-    },
-  });
+    // // Destroy all plugin instances created from the
+    // // e.g. $('input[type="range"]') elements.
+    // $('input[type="range"]').rangeslider('destroy');
+
+    // // Update all rangeslider instances for all
+    // // e.g. $('input[type="range"]') elements.
+    // // Usefull if you changed some attributes e.g. `min` or `max` etc.
+    // $('input[type="range"]').rangeslider('update', true);
 
 
-  window.setVol = val => {
-    for (let p of recordArray) {
-      p.volume.value = val;
-    }
-  }
 
-  window.delaytrig = delayval => {
-    for (let p of recordArray) {
-      delay.wet.value = delayval;
-      p.connect(delay)
-    }
-  }
+  // window.setVol = val => {
+  //   for (let p of recordArray) {
+  //     p.volume.value = val;
+  //   }
+  // }
 
-  window.reverbtrig = reverbval => {
-    for (let p of recordArray) {
-      reverb.wet.value = reverbval;
-      p.connect(reverb)
-    }
-  }
+  // window.delaytrig = delayval => {
+  //   for (let p of recordArray) {
+  //     delay.wet.value = delayval;
+  //     p.connect(delay)
+  //   }
+  // }
 
-})
+  // window.reverbtrig = reverbval => {
+  //   for (let p of recordArray) {
+  //     reverb.wet.value = reverbval;
+  //     p.connect(reverb)
+  //   }
+  // }
+});
