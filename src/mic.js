546aff3ff3b3bd4ac081c0e81eb9a631831d803f
cheesestick24 <ht17a075@oecu.jp>
2019-10-20 13:14:14 +0900

    進捗

--- src/mic.js
+++ src/mic.js
@@ -76,21 +76,6 @@ $(function() {
       }
     });
 
-    // // Initialize a new plugin instance for all
-    // // e.g. $('input[type="range"]') elements.
-    // $('input[type="range"]').rangeslider();
-
-    // // Destroy all plugin instances created from the
-    // // e.g. $('input[type="range"]') elements.
-    // $('input[type="range"]').rangeslider('destroy');
-
-    // // Update all rangeslider instances for all
-    // // e.g. $('input[type="range"]') elements.
-    // // Usefull if you changed some attributes e.g. `min` or `max` etc.
-    // $('input[type="range"]').rangeslider('update', true);
-
-
-
   // window.setVol = val => {
   //   for (let p of recordArray) {
   //     p.volume.value = val;
