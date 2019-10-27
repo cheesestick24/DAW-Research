71624c7d7b18aa813b758f7d672343b9ed590686
cheesestick24 <ht17a075@oecu.jp>
2019-10-27 16:27:59 +0900

    時間経過の更新

--- src/timer.js
+++ src/timer.js
@@ -9,13 +9,14 @@ export default {
             const milliSec = currentTime - oldTime;
 
             let sec = Math.floor(milliSec / 1000);
-            const min = Math.floor(sec / 60);
+            let min = Math.floor(sec / 60);
             let mil = milliSec - sec * 1000;
             if (sec >= 60) {
                 sec = sec % 60;
-                mil = milliSec - (sec * 1000) - 60000
+                mil = milliSec - (60000 * min) - (sec * 1000)
+                console.log(mil,sec,min)
             }
-            // console.log(min + "分" + sec + "秒" + mil);
+            console.log(min + "分" + sec + "秒" + mil);
             $("#timer").text("録音時間" + min + "分" + sec + "秒" + mil);
         }, 1);
     },
