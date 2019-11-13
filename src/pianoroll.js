800f7c03db40a34dbdf11c8b0eae2a6b8234e38b
ht17a075@oecu.jp <ht17a075@oecu.jp>
2019-11-13 14:21:57 +0900

    ピアノロールの正常化

--- src/pianoroll.js
+++ src/pianoroll.js
@@ -1,4 +1,6 @@
 import Tone from 'tone'
+
+let firstFlag
 const scale = [
     'C4',
     'D4',
@@ -14,7 +16,7 @@ const scale = [
     'G5',
     'A5',
     'B5',
-  ];
+];
 function playTone(index) {
     //鍵盤
     const synth = new Tone.Synth().toMaster();
@@ -23,30 +25,43 @@ function playTone(index) {
 
     synth.triggerAttackRelease(scale[index], '32n');
     // (time, 0, "32n", 0, vel);
-  }
+}
 
+const length = 16;
 
-
-
+firstFlag = true;
 var loop = new Tone.Sequence(function (time, col) {
     var column = document.querySelector("tone-step-sequencer").currentColumn;
     column.forEach(function (val, i) {
-        if (val) {
+        if (firstFlag && col == length - 1) {
+            firstFlag = null;
+            return false;
+        } else if (val) {
+            if (firstFlag && col == 0) {
+                firstFlag = null;
+                return false;
+            }
+            console.log(col)
+
             //slightly randomized velocities
             var vel = Math.random() * 0.5 + 0.5;
             playTone(i);
+        } else {
+            // firstFlag = null;
         }
     });
     //set the columne on the correct draw frame
     Tone.Draw.schedule(function () {
         document.querySelector("tone-step-sequencer").setAttribute("highlight", col);
     }, time);
-}, [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15], "16n").start(0);
+}, [...length], "16n").start(0, 0);
 
 //bind the interface
 document.querySelector("tone-transport").bind(Tone.Transport);
 
 Tone.Transport.on("stop", () => {
+    firstFlag = true;
+
     setTimeout(() => {
         document.querySelector("tone-step-sequencer").setAttribute("highlight", "-1");
     }, 100);
