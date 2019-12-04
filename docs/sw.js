d8588f76d94d91232b895840fa0177cdec27d237
ht17a075@oecu.jp <ht17a075@oecu.jp>
2019-12-04 16:33:08 +0900

    aaa

--- docs/sw.js
+++ docs/sw.js
@@ -1,32 +1,38 @@
-var CACHE_DYNAMIC_VERSION = 'dynamic-v1';
 
-self.addEventListener('fetch', function(event) {
-  console.log('[Service Worker] Fetching something ...');
-  event.respondWith(
-    // キャッシュの存在チェック
-    caches.match(event.request)
-      .then(function(response) {
-        if (response) {
-          return response;
-        } else {
-          // キャッシュがなければリクエストを投げて、レスポンスをキャッシュに入れる
-          return fetch(event.request)
-            .then(function(res) {
-              return caches.open(CACHE_DYNAMIC_VERSION)
-                .then(function(cache) {
-                  // 最後に res を返せるように、ここでは clone() する必要がある
-                  console.log("sasda");
-                  cache.put(event.request.url, res.clone());
-                  return res;
-                })
-            })
-            .catch(function() {
-              // エラーが発生しても何もしない
-            });
-        }
-      })
-  );
-});
+
+self.addEventListener('fetch', function(){});
+
+
+
+// var CACHE_DYNAMIC_VERSION = 'dynamic-v1';
+
+// self.addEventListener('fetch', function(event) {
+//   console.log('[Service Worker] Fetching something ...');
+//   event.respondWith(
+//     // キャッシュの存在チェック
+//     caches.match(event.request)
+//       .then(function(response) {
+//         if (response) {
+//           return response;
+//         } else {
+//           // キャッシュがなければリクエストを投げて、レスポンスをキャッシュに入れる
+//           return fetch(event.request)
+//             .then(function(res) {
+//               return caches.open(CACHE_DYNAMIC_VERSION)
+//                 .then(function(cache) {
+//                   // 最後に res を返せるように、ここでは clone() する必要がある
+//                   console.log("sasda");
+//                   cache.put(event.request.url, res.clone());
+//                   return res;
+//                 })
+//             })
+//             .catch(function() {
+//               // エラーが発生しても何もしない
+//             });
+//         }
+//       })
+//   );
+// });
 
 // キャッシュファイルの指定
 // var CACHE_NAME = 'pwa-sample-caches';
